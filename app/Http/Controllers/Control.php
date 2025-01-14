1ff38cba11cae3d6cf119033420e4648ec83ef79
navi931 <47607235+navi931@users.noreply.github.com>
2025-01-14 10:45:14 -0600

    first commit

---
+++ app/Http/Controllers/Control.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \stdClass;
+use \DateTime;
+use Illuminate\Support\Facades\DB;
+use \App\object_sorter;
+use Response;
+use \App\Calculos;
+use \App\Conversiones;
+use Mail;
+use \App\Mail\enviarCorreo;
+use \App\Mail\enviarCorreoCliente;
+#409 error de token
+
+class Control extends Controller
+{
+  
+}
